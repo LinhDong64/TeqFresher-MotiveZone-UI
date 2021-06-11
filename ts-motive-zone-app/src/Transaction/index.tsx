558f69d97bd715094e7879d36dc6a7760488c149
LinhDong64 <mailinhdong@gmail.com>
2021-06-11 11:42:33 +0700

    create total-section(transaction)

--- ts-motive-zone-app/src/Transaction/index.tsx
+++ ts-motive-zone-app/src/Transaction/index.tsx
@@ -23,12 +23,43 @@ export default function Transaction() {
   }
 
   return (
-    <div>
-      <button onClick={handleOpenPicker}>Open Picker</button>
-      <div>{monthYear.month}-{monthYear.year}</div>
-      <MonthYearPicker isOpen={isOpenPicker}
-        closePicker={handleClosePicker}
-        getMonthYear={handleGetMonthYear} />
+    <div className="trans">
+      <div className="trans-container">
+        <div className="trans__header">
+          <span>利用履歴</span>
+          <span>月ごとの支出グラフを隠す</span>
+        </div>
+        <div className="trans__total">
+          <div className="month-picker">
+            <span className="month">
+              {monthYear.month}<span>月</span>
+            </span>
+            <span className="chevron-icon" onClick={handleOpenPicker}>
+              <i className='fa fa-chevron-down'
+              style={{ fontSize: '15px', color: 'gray' }}></i>
+            </span>
+            <MonthYearPicker isOpen={isOpenPicker}
+              closePicker={handleClosePicker}
+              getMonthYear={handleGetMonthYear} />
+          </div>
+          <div className="total__statistic">
+            <div className="expense-group">
+              <div className="expense">
+                <span><b>収入</b></span>
+                <span><b>0 円</b></span>
+              </div>
+              <hr></hr>
+            </div>
+            <div className="income-group">
+              <div className="income">
+                <span><b>ご利用額</b></span>
+                <span><b>0 円</b></span>
+              </div>
+              <hr></hr>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
