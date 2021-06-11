b29e24df67846d663df5cce6cfff833602937e17
LinhDong64 <mailinhdong@gmail.com>
2021-06-11 17:27:13 +0700

    update scss folder- config react chart

--- ts-motive-zone-app/src/Transaction/Chart/index.tsx
+++ ts-motive-zone-app/src/Transaction/Chart/index.tsx
@@ -1,61 +1,59 @@
-
-import { BarChart, Bar, Cell, XAxis, YAxis, CartesianAxis, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+import {
+  BarChart, Bar, Tooltip, LabelList, ResponsiveContainer, CartesianGrid,
+  Label, XAxis
+} from 'recharts';
 
 
 export default function Chart() {
   const data = [
     {
       name: '1',
-      uv: 4000,
       pv: 2400,
       amt: 2400,
     },
     {
       name: '2',
-      uv: 3000,
       pv: 1398,
       amt: 2210,
     },
     {
       name: '3',
-      uv: 2000,
       pv: 9800,
       amt: 2290,
     },
     {
       name: '4',
-      uv: 2780,
       pv: 3908,
       amt: 2000,
     },
     {
       name: '5',
-      uv: 1890,
       pv: 4800,
       amt: 2181,
     },
     {
       name: '6',
-      uv: 2390,
       pv: 3800,
       amt: 2500,
     },
     {
       name: '12',
-      uv: 3490,
       pv: 4300,
       amt: 2100,
     },
   ];
+  
   return (
-    <BarChart width={730} height={250} data={data}>
-      <CartesianAxis strokeDasharray=""/>
-      <XAxis dataKey="name" orientation='top' />
-      <YAxis />
-      <Tooltip />
-      <Legend />
-      <Bar dataKey="pv" fill="#8884d8">
-      </Bar> 
-    </BarChart>
+    <ResponsiveContainer width="100%" height={250}>
+      <BarChart data={data} margin={{ top: 15, right: 30, left: 20, bottom: 5 }}>
+        <CartesianGrid verticalPoints={[20,105, 191, 277, 363, 448, 534,620]}
+          horizontalPoints={[250]} />
+        <Tooltip />
+        <XAxis dataKey="name" orientation="top">
+          <Label value="2021" offset={0} position="insideTopLeft"/>
+        </XAxis>
+        <Bar dataKey="pv" fill="#BFD2ED"/>
+      </BarChart>
+    </ResponsiveContainer>
   )
 }
--- ts-motive-zone-app/src/services/callAPI.ts
+++ ts-motive-zone-app/src/services/callAPI.ts
@@ -3,8 +3,27 @@ import axios from 'axios'
 export const sendContactForm = (data: any) => {
   return axios({
     method: 'post',
-    url: ' https://mock.stg.offwork.teqnological.asia/mock/6092029dfdc7510021577547/contact',
+    url: `https://mock.stg.offwork.teqnological.asia/
+          mock/6092029dfdc7510021577547/contact`,
     data: { data }
+  }).then(res => {
+    return res.data;
+  })
+}
+
+export const getTransactionData = () => {
+  return axios({
+    method: 'get',
+    url: `https://mock.stg.offwork.teqnological.asia/mock/6092029dfdc7510021577547/xxxx/transaction`
+  }).then(res => {
+    return res.data;
+  })
+}
+
+export const getDataForTransChart = () => {
+  return axios({
+    method: 'get',
+    url: `https://mock.stg.offwork.teqnological.asia/mock/6092029dfdc7510021577547/xxxx/chart`
   }).then(res => {
     return res.data;
   })
