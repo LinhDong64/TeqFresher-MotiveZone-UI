b097aa7a33d55f604edd4e749e6fd608efd75e03
LinhDong64 <mailinhdong@gmail.com>
2021-06-18 12:53:27 +0700

    scroll chart

--- ts-motive-zone-app/src/Transaction/Chart/index.tsx
+++ ts-motive-zone-app/src/Transaction/Chart/index.tsx
@@ -1,13 +1,12 @@
+import { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 import {
-  BarChart, Bar, Tooltip, ResponsiveContainer,
+  BarChart, Bar, Tooltip,
   Label, XAxis, ReferenceLine, YAxis
 } from 'recharts';
 import formatDataChart from '../../utils/function/formatDataChart'
 
 export default function Chart(transProps: any) {
-  console.log('time', transProps);
-
   const state: any = useSelector(state => state);
   const chartData = state.transaction.chartData.values;
   const data = formatDataChart(chartData, transProps.time.year);
@@ -23,11 +22,10 @@ export default function Chart(transProps: any) {
   }
 
   const CustomTick = (props: any) => {
-    console.log('tick', typeof (props.payload.value));
     if (props.payload.value === transProps.time.month) {
       return (
         <g transform={`translate(${props.x},${props.y})`} >
-          <text textAnchor="end" fontSize={12} stroke='#2D69C4'>
+          <text textAnchor="end" fontSize={15} stroke='#2D69C4'>
             {props.payload.value}
           </text>
         </g>
@@ -35,7 +33,7 @@ export default function Chart(transProps: any) {
     } else {
       return (
         <g transform={`translate(${props.x},${props.y})`} >
-          <text textAnchor="end" fontSize={12}>
+          <text textAnchor="end" fontSize={15}>
             {props.payload.value}
           </text>
         </g>
@@ -43,39 +41,68 @@ export default function Chart(transProps: any) {
     }
   }
 
+  useEffect(() => {
+    const slider = document.querySelector('.items') as HTMLElement;
+    let startX: any;
+    let scrollLeft: any;
+    let isDown = false;
+
+    slider?.addEventListener('mousedown', ((e: MouseEvent) => {
+      isDown = true;
+      console.log(isDown);
+      slider.classList.add('active');
+      startX = e.pageX - slider.offsetLeft;
+      scrollLeft = slider.scrollLeft;
+    }) as EventListener);
+    slider?.addEventListener('mouseleave', (() => {
+      isDown = false;
+    }) as EventListener);
+    slider?.addEventListener('mouseup', (() => {
+      isDown = false;
+    }) as EventListener);
+    slider?.addEventListener('mousemove', ((e: MouseEvent) => {
+      if (!isDown) return;
+      e.preventDefault();
+      const x = e.pageX - slider.offsetLeft;
+      const walk = (x - startX) * 3; //scroll-fast
+      slider.scrollLeft = scrollLeft - walk;
+    }) as EventListener);
+  }, []);
+
   const colorLine = "#808080";
   return (
-    <div>
-      <ResponsiveContainer width="100%" height={250}>
-        <BarChart data={data} margin={{ top: 15, bottom: 5 }}>
-          <Tooltip />
-          <XAxis dataKey="month" orientation="top" tickLine={false}
-            axisLine={false} tick={<CustomTick />}>
-            <Label content={renderLabel} ></Label>
-          </XAxis>
-          <YAxis reversed={true} hide />
-          <Bar dataKey="value" fill="#BFD2ED" radius={10} />
-          {data.map((item: any) => (
-            <ReferenceLine
-              ifOverflow="visible"
-              key={item.time}
-              x={item.month}
-              stroke={colorLine}
-              position="start"
-              className='custome-reference-line'
-            />
-          ))}
-          {data.length > 0 && (
-            <ReferenceLine
-              ifOverflow="visible"
-              x={data[data.length - 1].month}
-              stroke={colorLine}
-              position="end"
-              className='custome-reference-line'
-            />
-          )}
-        </BarChart>
-      </ResponsiveContainer>
+    <div className='items'>
+      <BarChart data={data} margin={{ top: 15, bottom: 5 }}
+        width={730} height={250}
+        style={{ cursor: 'grab'}}>
+        <Tooltip />
+        <XAxis dataKey="month" orientation="top"
+          tickLine={false} axisLine={false} tick={<CustomTick />}>
+          <Label content={renderLabel} ></Label>
+        </XAxis>
+        <YAxis reversed={true} hide />
+        <Bar dataKey="value" fill="#BFD2ED" radius={10}
+          barSize={75} />
+        {data.map((item: any) => (
+          <ReferenceLine
+            ifOverflow="visible"
+            key={item.time}
+            x={item.month}
+            stroke={colorLine}
+            position="start"
+            className='custome-reference-line'
+          />
+        ))}
+        {data.length > 0 && (
+          <ReferenceLine
+            ifOverflow="visible"
+            x={data[data.length - 1].month}
+            stroke={colorLine}
+            position="end"
+            className='custome-reference-line'
+          />
+        )}
+      </BarChart>
     </div>
   )
 }
--- ts-motive-zone-app/src/services/callAPI.ts
+++ ts-motive-zone-app/src/services/callAPI.ts
@@ -11,20 +11,149 @@ export const sendContactForm = (data: any) => {
 }
 
 export const getTransactionData = () => {
-  return axios({
-    method: 'get',
-    url: `https://mock.stg.offwork.teqnological.asia/mock/6092029dfdc7510021577547/xxxx/transaction`
-  }).then(async res => {
-    return res.data.data;
-  })
+  // return axios({
+  //   method: 'get',
+  //   url: `https://mock.stg.offwork.teqnological.asia/mock/6092029dfdc7510021577547/xxxx/transaction`
+  // }).then(async res => {
+  //   return res.data.data;
+  // })
+  return {
+    message:"200 OK"
+  }
 
 }
 
 export async function getDataForChart() {
- return await axios({
-    method: 'get',
-    url: `https://mock.stg.offwork.teqnological.asia/mock/6092029dfdc7510021577547/xxxx/chart`
-  }).then( res => {
-    return res.data.data;
-  })
+//  return await axios({
+//     method: 'get',
+//     url: `https://mock.stg.offwork.teqnological.asia/mock/6092029dfdc7510021577547/xxxx/chart`
+//   }).then( res => {
+//     return res.data.data;
+//   })
+  return {
+    values:[
+      {
+        time:'2020-03', 
+        value: '-100000'
+      },
+      {
+        time:'2020-04', 
+        value: '0'
+      },
+      {
+        time:'2020-05', 
+        value: '-200000'
+      },
+      {
+        time:'2020-06', 
+        value: '0'
+      },
+      {
+        time:'2020-07', 
+        value: '-10000'
+      },
+      {
+        time:'2020-08', 
+        value: '0'
+      },
+      {
+        time:'2020-09', 
+        value: '0'
+      }, 
+      {
+        time:'2020-10', 
+        value: '-390000'
+      },
+      {
+        time:'2020-11', 
+        value: '0'
+      },
+
+      {
+        time:'2021-01', 
+        value: '0'
+      },
+      {
+        time:'2021-02', 
+        value: '-100000'
+      },
+      {
+        time:'2021-03', 
+        value: '-100000'
+      },
+      {
+        time:'2021-04', 
+        value: '-54000'
+      },
+      {
+        time:'2021-05', 
+        value: '-200000'
+      },
+      {
+        time:'2021-06', 
+        value: '-218370'
+      },
+      {
+        time:'2021-07', 
+        value: '-10000'
+      },
+      {
+        time:'2021-08', 
+        value: '-628920'
+      },
+      {
+        time:'2021-09', 
+        value: '-322080'
+      }, 
+      {
+        time:'2021-10', 
+        value: '-390000'
+      },
+      {
+        time:'2021-11', 
+        value: '-320000'
+      },
+
+      // {
+      //   time:'2020-01', 
+      //   value: '0'
+      // },
+      // {
+      //   time:'2020-02', 
+      //   value: '-100000'
+      // },
+      // {
+      //   time:'2020-03', 
+      //   value: '-200000'
+      // },
+      // {
+      //   time:'2020-04', 
+      //   value: '0'
+      // },
+      // {
+      //   time:'2020-05', 
+      //   value: '0'
+      // },
+      // {
+      //   time:'2020-06', 
+      //   value: '-253000'
+      // },
+      // {
+      //   time:'2020-07', 
+      //   value: '-200000'
+      // },
+      // {
+      //   time:'2020-08', 
+      //   value: '0'
+      // },
+      // {
+      //   time:'2020-09', 
+      //   value: '0'
+      // },
+      // {
+      //   time:'2020-10', 
+      //   value: '-253000'
+      // }
+    ]
+  }
 }
