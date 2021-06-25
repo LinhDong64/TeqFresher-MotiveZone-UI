834132b9aa05890101464c06c20e5998c31692cf
LinhDong64 <mailinhdong@gmail.com>
2021-06-25 11:08:43 +0700

    update scroll chart

--- ts-motive-zone-app/src/Transaction/Chart/index.tsx
+++ ts-motive-zone-app/src/Transaction/Chart/index.tsx
@@ -23,52 +23,54 @@ export default function Chart(transProps: any) {
 
   const CustomTick = (props: any) => {
     let color = props.payload.value === transProps.time.month ? '#2D69C4' : '';
-      return (
-        <g transform={`translate(${props.x},${props.y})`} >
-          <text textAnchor="end" fontSize={15} stroke={color}>
-            {props.payload.value}
-          </text>
-        </g>
-      )
+    return (
+      <g transform={`translate(${props.x},${props.y})`} >
+        <text textAnchor="end" fontSize={15} stroke={color}>
+          {props.payload.value}
+        </text>
+      </g>
+    )
   }
 
+  let startX: any;
+  let scrollLeft: any;
+  let isDown = false;
+  function handleMouseDownSlider(e: MouseEvent, slider: any) {
+    isDown = true;
+    console.log(isDown);
+    slider.classList.add('active');
+    startX = e.pageX - slider.offsetLeft;
+    scrollLeft = slider.scrollLeft;
+  }
+
+  function handleMouseLeaveSlider() {
+    isDown = false;
+  }
+
+  function handleMouseUpSlider() {
+    isDown = false;
+  }
+
+  function handleMouseMoveSlider(e: MouseEvent, slider: any) {
+    if (!isDown) return;
+    e.preventDefault();
+    const x = e.pageX - slider.offsetLeft;
+    const walk = (x - startX) * 3; //scroll-fast
+    slider.scrollLeft = scrollLeft - walk;
+  }
   useEffect(() => {
     const slider = document.querySelector('.items') as HTMLElement;
-    let startX: any;
-    let scrollLeft: any;
-    let isDown = false;
+    slider?.addEventListener('mousedown',
+      function (e: MouseEvent) { handleMouseDownSlider(e, slider) } as EventListener);
 
-    function handleMouseDownSlider(e: MouseEvent){
-      isDown = true;
-      console.log(isDown);
-      slider.classList.add('active');
-      startX = e.pageX - slider.offsetLeft;
-      scrollLeft = slider.scrollLeft;
-    } 
+    slider?.addEventListener('mouseleave',
+      handleMouseLeaveSlider as EventListener);
 
-    function handleMouseLeaveSlider(){
-      isDown = false;
-    }
+    slider?.addEventListener('mouseup',
+      handleMouseUpSlider as EventListener);
 
-    function handleMouseUpSlider(){
-      isDown = false;
-    }
-
-    function handleMouseMoveSlider(e: MouseEvent){
-      if (!isDown) return;
-      e.preventDefault();
-      const x = e.pageX - slider.offsetLeft;
-      const walk = (x - startX) * 3; //scroll-fast
-      slider.scrollLeft = scrollLeft - walk;
-    }
-
-    slider?.addEventListener('mousedown', handleMouseDownSlider as EventListener);
-
-    slider?.addEventListener('mouseleave', handleMouseLeaveSlider as EventListener);
-    
-    slider?.addEventListener('mouseup', handleMouseUpSlider as EventListener);
-
-    slider?.addEventListener('mousemove', handleMouseMoveSlider as EventListener);
+    slider?.addEventListener('mousemove',
+      function (e: MouseEvent) { handleMouseMoveSlider(e, slider) } as EventListener);
   }, []);
 
   const colorLine = "#808080";
@@ -76,7 +78,7 @@ export default function Chart(transProps: any) {
     <div className='items'>
       <BarChart data={data} margin={{ top: 15, bottom: 5 }}
         width={730} height={250}
-        style={{ cursor: 'grab'}}>
+        style={{ cursor: 'grab' }}>
         <Tooltip />
         <XAxis dataKey="month" orientation="top"
           tickLine={false} axisLine={false} tick={<CustomTick />}>
